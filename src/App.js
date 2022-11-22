05dd0d4b086516060553a100fd3fadbec471350d
xxAdmGenxx <73734698+xxAdmGenxx@users.noreply.github.com>
2022-11-22 00:16:59 -0800

    See discord for list of edits.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Welcome from './Welcome';
-import CreatePollForm from './CreatePollForm';
+import Tabs from "./FormsAndTabs/Tabs";
+import ScheduleForm from './FormsAndTabs/ScheduleForm';
 
 function App() {
   return (
-    <>
+    <div>
       <Welcome />
-      <CreatePollForm />
-    </>
+      <Tabs />
+    </div>
   );
 }
 
